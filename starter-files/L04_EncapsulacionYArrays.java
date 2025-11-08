5c2d80188e13522de64a572c1e7ccf3b74f478fb
Manuel Fernando Joao Castro Picon <yonyugmer8@gmail.com>
2025-11-07 22:51:23 -0500

    Create L04_EncapsulacionYArrays.java

---
+++ starter-files/L04_EncapsulacionYArrays.java
@@ -0,0 +1,21 @@
+// TODO: Crea una clase Estudiante con atributos privados y métodos getter/setter
+// TODO: Crea un array de estudiantes y recórrelo para mostrar su información
+// TODO: Actualiza la edad de uno de los estudiantes
+
+class Estudiante {
+    // Atributos privados: nombre, edad
+
+    // Constructor
+
+    // Métodos: getNombre, setEdad, mostrarInfo
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Crea un array con 3 estudiantes
+
+        // Recorre el array y muestra la información
+
+        // Actualiza la edad del último estudiante y muestra el cambio
+    }
+}

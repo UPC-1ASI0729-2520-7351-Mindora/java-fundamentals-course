369c97e29a166d80892d52d92b31f931643c0db6
Manuel Fernando Joao Castro Picon <yonyugmer8@gmail.com>
2025-11-07 22:50:48 -0500

    Add initial structure for operators and control flow
    Initial implementation of arithmetic operators, conditionals, and loops practice.

---
+++ starter-files/L02_OperadoresYControl.java
@@ -0,0 +1,19 @@
+// TODO: Practica operadores aritméticos, condicionales y bucles
+
+public class Main {
+    public static void main(String[] args) {
+        // Declara dos variables enteras: edad y nota
+
+        // Realiza operaciones: suma, resta, multiplicación, división, módulo
+
+        // Imprime los resultados
+
+        // TODO: Usa una estructura if-else para evaluar si la nota es aprobatoria
+
+        // TODO: Crea un bucle for que imprima del 1 al 5
+
+        // TODO: Crea un bucle while que imprima del 1 al 5
+
+        // TODO: Combina bucle + condicional para imprimir si un número es par o impar
+    }
+}

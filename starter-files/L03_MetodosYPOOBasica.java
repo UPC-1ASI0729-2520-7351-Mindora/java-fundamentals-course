061541ab78cafdd9fe112b9f3c0cbbe283c7b452
Manuel Fernando Joao Castro Picon <yonyugmer8@gmail.com>
2025-11-07 22:51:03 -0500

    Create basic structure for greeting and student class
    Implemented a basic structure for greeting and student information.

---
+++ starter-files/L03_MetodosYPOOBasica.java
@@ -0,0 +1,27 @@
+// TODO: Crea un método que salude con el nombre del usuario
+// TODO: Usa Scanner para pedir nombre y edad
+// TODO: Crea una clase Estudiante con atributos y método mostrarInfo
+
+import java.util.Scanner;
+
+class Estudiante {
+    // Atributos: nombre, edad
+
+    // Constructor
+
+    // Método mostrarInfo
+}
+
+public class Main {
+    public static void saludarConNombre(String nombre) {
+        // Imprime un saludo personalizado
+    }
+
+    public static void main(String[] args) {
+        // Pide nombre y edad por consola
+
+        // Llama al método saludarConNombre
+
+        // Crea un objeto Estudiante y muestra su información
+    }
+}

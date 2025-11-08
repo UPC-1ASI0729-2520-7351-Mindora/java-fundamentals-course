cfbba6e6f0e7c58bfd234bdb6277c2d2efbc8a16
Manuel Fernando Joao Castro Picon <yonyugmer8@gmail.com>
2025-11-07 22:51:40 -0500

    Create Estudiante class and main program structure
    Implement class Estudiante with encapsulation and methods. Use Scanner to register 3 students and update the age of the second student.

---
+++ starter-files/L05_ProgramaCompletoYBuenasPracticas.java
@@ -0,0 +1,25 @@
+// TODO: Crea una clase Estudiante con encapsulación y métodos completos
+// TODO: Usa Scanner para registrar 3 estudiantes desde consola
+// TODO: Muestra la lista completa y actualiza la edad del segundo estudiante
+
+import java.util.Scanner;
+
+class Estudiante {
+    // Atributos privados: nombre, edad
+
+    // Constructor
+
+    // Métodos: getNombre, getEdad, setEdad, mostrarInfo
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Crea un array de 3 estudiantes
+
+        // Pide nombre y edad por consola para cada estudiante
+
+        // Muestra la lista completa
+
+        // Actualiza la edad del segundo estudiante y muestra el cambio
+    }
+}
